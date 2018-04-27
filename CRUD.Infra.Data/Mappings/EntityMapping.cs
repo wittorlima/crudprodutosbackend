ca44ed1b94eef4c08d6f409a6ced303ef431f903
WITTOR LIMA <wittor@wslsistemas.com>
2018-04-27 17:42:51 -0400

    Mapeamento entidade produto.

---
+++ CRUD.Infra.Data/Mappings/EntityMapping.cs
@@ -0,0 +1,25 @@
+﻿using CRUD.Domain.Entities;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Infra.Data.Mappings
+{
+    public class EntityMapping<T> : ClassMapping<T> where T : ModelBase
+    {
+        public EntityMapping()
+        {
+            Schema("emax");
+
+            Id(x => x.Id, mapper =>
+            {
+                mapper.Generator(Generators.Increment);
+                mapper.Column("id");
+            });
+        }
+    }
+}
---
+++ CRUD.Infra.Data/Mappings/ProductMapping.cs
@@ -0,0 +1,43 @@
+﻿
+using CRUD.Domain.Entities;
+
+namespace CRUD.Infra.Data.Mappings
+{
+    public class ProductMapping : EntityMapping<Product>
+    {
+        public ProductMapping()
+        {
+            Table("Product");
+
+            Property(x => x.Description, mapper =>
+            {
+                mapper.Column("Description");
+                mapper.Length(120);
+                mapper.NotNullable(false);
+            });
+
+            Property(x => x.DatePurchased, mapper =>
+            {
+                mapper.Column("DatePurchased");
+                mapper.Length(120);
+                mapper.NotNullable(true);
+            });
+
+            Property(x => x.Image, mapper =>
+            {
+                mapper.Column("Image");
+                mapper.NotNullable(false);
+            });
+
+            Property(x => x.Origin, mapper =>
+            {
+                mapper.Column("Origin");
+            });
+
+            Property(x => x.Price, mapper =>
+            {
+                mapper.Column("Price");
+            });
+        }
+    }
+}
