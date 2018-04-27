01f43b24184dafa69e12a994f1876af03a65a12c
WITTOR LIMA <wittor@wslsistemas.com>
2018-04-27 15:25:52 -0400

    Criação das entidades

---
+++ CRUD.Domain/CRUD.Domain/Constants/Enums.cs
@@ -0,0 +1,9 @@
+﻿
+namespace CRUD.Domain.Constants
+{
+    public enum Origem
+    {
+        Nacional = 0,
+        Importado = 1,
+    }
+}
---
+++ CRUD.Domain/CRUD.Domain/Entities/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Domain.Entities
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string Descricao { get; set; }
+    }
+}
