2f7bda2823e94ec87a7b2a5bae2fbbbb0c6411c5
WITTOR LIMA <wittor@wslsistemas.com>
2018-04-28 02:50:23 -0400

    Modulos

---
+++ CRUD.Application/Modules/ModuleBase.cs
@@ -0,0 +1,62 @@
+﻿using Nancy;
+using Nancy.Responses.Negotiation;
+using Newtonsoft.Json;
+using System;
+
+namespace CRUD.Application.Modules
+{
+    public abstract class ModuleBase : NancyModule
+    {
+        public ModuleBase(string modulePath)
+            : base(modulePath)
+        {
+            RegisterRoutes();
+        }
+
+        protected abstract void RegisterRoutes();
+
+        public Negotiator ExceptionResponse(Exception exception)
+        {
+            return ExceptionResponse(exception, HttpStatusCode.BadRequest);
+        }
+
+        public Negotiator ExceptionResponse(Exception exception, HttpStatusCode statusCode)
+        {
+            string message = statusCode == HttpStatusCode.InternalServerError ? "An error has occurred in our servers" : exception.Message;
+            return Negotiate
+                .WithModel(message)
+                .WithStatusCode(statusCode);
+        }
+
+        #region Assistants methods
+
+        protected T DeserializeObject<T>(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<T>(jsonString);
+        }
+
+        public string SerializeObject(object model)
+        {
+            return JsonConvert.SerializeObject(model,
+                            Formatting.None,
+                            new JsonSerializerSettings()
+                            {
+                                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                                DateFormatString = "yyyy-MM-ddTHH:mmZ"
+                            });
+        }
+
+        public string SerializeObjectWithoutTimeZone(object model)
+        {
+            return JsonConvert.SerializeObject(model,
+                            Formatting.None,
+                            new JsonSerializerSettings()
+                            {
+                                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                                DateFormatString = "yyyy-MM-ddTHH:mm"
+                            });
+        }
+
+        #endregion
+    }
+}
---
+++ CRUD.Application/Modules/ProductModule.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using CRUD.Application.DTOs;
+using CRUD.Domain.Entities;
+using CRUD.Domain.Exceptions;
+using CRUD.Domain.Interfaces.Services;
+using Nancy;
+using Nancy.ModelBinding;
+using System;
+
+namespace CRUD.Application.Modules
+{
+    public class ProductModule : ModuleBase
+    {
+        public IProductService ProductService;
+
+        public ProductModule(IProductService productService)
+            : base("/products")
+        {
+            ProductService = productService;
+        }
+
+        protected override void RegisterRoutes()
+        {
+            Get["/"] =  Find;
+            Get["/{id}"] = FindById;
+            Post["/"] = Save;
+        }
+
+        private dynamic Find(dynamic parameters)
+        {
+            try
+            {
+                var resultResponse = ProductService.GetAll();
+
+                return this.SerializeObject(resultResponse);
+            }
+            catch (Exception exception)
+            {
+                if (exception is InvalidParameterException)
+                    return ExceptionResponse(exception, HttpStatusCode.BadRequest);
+                return ExceptionResponse(exception, HttpStatusCode.InternalServerError);
+            }
+        }
+
+        private dynamic FindById(dynamic parameters)
+        {
+            try
+            {
+                var id = (int)parameters.Id;
+                var product = ProductService.GetById(id);
+                var productDTO = Mapper.Map<ProductDTO>(product);
+
+                return this.SerializeObjectWithoutTimeZone(productDTO);
+            }
+            catch (Exception exception)
+            {
+                if (exception is InvalidParameterException)
+                    return ExceptionResponse(exception, HttpStatusCode.BadRequest);
+
+                return ExceptionResponse(exception, HttpStatusCode.InternalServerError);
+            }
+        }
+
+        private dynamic Save(dynamic parameters)
+        {
+            try
+            {
+                var ProductDTO = this.Bind<ProductDTO>();
+                var product = Mapper.Map<Product>(ProductDTO);
+
+                ProductService.SaveOrUpdate(product);
+
+                return this.SerializeObject(ProductDTO);
+            }
+            catch (Exception exception)
+            {
+                if (exception is InvalidParameterException)
+                    return ExceptionResponse(exception, HttpStatusCode.BadRequest);
+
+                return ExceptionResponse(exception, HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
