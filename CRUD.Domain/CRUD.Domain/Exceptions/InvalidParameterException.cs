913b8c8f4cadd94a21438565d9423f9396325a45
WITTOR LIMA <wittor@wslsistemas.com>
2018-04-28 02:50:51 -0400

    Excessão parametros

---
+++ CRUD.Domain/CRUD.Domain/Exceptions/InvalidParameterException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Domain.Exceptions
+{
+    public class InvalidParameterException : Exception
+    {
+        public InvalidParameterException() { }
+        public InvalidParameterException(string message) : base(message) { }
+        public InvalidParameterException(string message, Exception inner) : base(message, inner) { }
+        protected InvalidParameterException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
