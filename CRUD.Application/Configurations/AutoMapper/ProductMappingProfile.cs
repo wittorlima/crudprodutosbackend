375457685750fc8d00f69f546f37270f2628cbbd
WITTOR LIMA <wittor@wslsistemas.com>
2018-04-28 02:49:57 -0400

    Configuração e mapeamento utilizando automapper

---
+++ CRUD.Application/Configurations/AutoMapper/ProductMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CRUD.Application.DTOs;
+using CRUD.Domain.Entities;
+
+namespace CRUD.Application.Configurations.AutoMapper
+{
+    public class ProductMappingProfile : Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<Product, ProductDTO>()
+                  .ForMember(x => x.Id, opt => opt.MapFrom(x => x.Id))
+                  .ForMember(x => x.Description, opt => opt.MapFrom(x => x.Description))
+                  .ForMember(x => x.DatePurchased, opt => opt.MapFrom(x => x.DatePurchased))
+                  .ForMember(x => x.Image, opt => opt.MapFrom(x => x.Image))
+                  .ForMember(x => x.Origin, opt => opt.MapFrom(x => x.Origin))
+                  .ForMember(x => x.Price, opt => opt.MapFrom(x => x.Price));
+        }
+    }
+}
---
+++ CRUD.Application/Configurations/AutoMapperBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CRUD.Application.Configurations.AutoMapper;
+
+namespace CRUD.Application.Configurations
+{
+    public class AutoMapperBootstrapper
+    {
+        public static void Initialize()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.AddProfile<ProductMappingProfile>();
+            });
+        }
+    }
+}
