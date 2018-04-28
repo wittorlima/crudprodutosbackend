4b2d846c0036652cbcd5b07cc5b1966248148d4b
WITTOR LIMA <wittor@wslsistemas.com>
2018-04-28 02:49:17 -0400

    Criação DTO

---
+++ CRUD.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,13 @@
+﻿using CRUD.Domain.Constants;
+using System;
+
+namespace CRUD.Application.DTOs
+{
+    public class ProductDTO : BaseDTO
+    {
+        public DateTime DatePurchased { get; set; }
+        public byte[] Image { get; set; }
+        public decimal Price { get; set; }
+        public Origin Origin { get; set; }
+    }
+}
